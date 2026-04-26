aceda0c342cfbe91d3a15c01b555879472b41689
mylongcode <mylongcode@gmail.com>
2026-04-26 22:01:47 +0500

    фикс

--- optimal_cutting_backend/optimal_cutting_backend/Services/DXFService.cs
+++ optimal_cutting_backend/optimal_cutting_backend/Services/DXFService.cs
@@ -4,11 +4,7 @@ using netDxf.Entities;
 using netDxf.Header;
 using netDxf.IO;
 using SkiaSharp;
-using System.Globalization;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
 using vega.Controllers.DTO;
 using vega.Migrations.DAL;
 using vega.Models;
@@ -18,19 +14,13 @@ namespace vega.Services
 {
     public class DXFService : IDXFService
     {
-        private readonly HttpClient _httpClient;
-
-        public DXFService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
         public async Task<List<byte[]>> Create2DDXFAsync(Cutting2DResult result)
         {
             var answer = new List<byte[]>();
             foreach (var item in result.Workpieces)
             {
                 var dxf = new DxfDocument();
-                foreach(var detail in item.Details)
+                foreach (var detail in item.Details)
                 {
                     dxf.Entities.Add(new Line(new Vector2(detail.X, detail.Y), new Vector2(detail.X + detail.Width, detail.Y)));
                     dxf.Entities.Add(new Line(new Vector2(detail.X, detail.Y), new Vector2(detail.X, detail.Y + detail.Height)));
@@ -58,9 +48,7 @@ namespace vega.Services
                 {
                     foreach (var figure in detail.Figures)
                     {
-                        var coorditanes = figure.Coordinates.Split(';').Select(f => float.Parse(f, new CultureInfo("ru-RU"))).ToList();
-                        var sizes = new Vector2(detail.Width / 2, -detail.Height / 2);
-                        var center = GetDetailCenter(detail.Figures);
+                        var coorditanes = figure.Coordinates.Split(';').Select(f => float.Parse(f)).ToList();
                         if (figure.TypeId == 1)
                         {
                             var start = new Vector2(coorditanes[0], coorditanes[1]);
@@ -69,39 +57,11 @@ namespace vega.Services
                             {
                                 (start.X, start.Y) = (-start.Y, start.X);
                                 (finish.X, finish.Y) = (-finish.Y, finish.X);
-                                (center.X, center.Y) = (-center.Y, center.X);
                             }
-                            start += new Vector2(detail.X, -detail.Y) + sizes - center;
-                            finish += new Vector2(detail.X, -detail.Y) + sizes - center;
-
+                            var center = GetDetailCenter(detail.Figures, detail.X, detail.Y);
+                            start += center;
+                            finish += center;
                             dxf.Entities.Add(new Line(start, finish));
-                        }
-                        if (figure.TypeId == 2)
-                        {
-                            var start = new Vector2(coorditanes[0], coorditanes[1]);
-                            var radius = coorditanes[2];
-                            if (detail.Rotated)
-                            {
-                                (start.X, start.Y) = (-start.Y, start.X);
-                                (center.X, center.Y) = (-center.Y, center.X);
-                            }
-                            start += new Vector2(detail.X, -detail.Y) + sizes - center;
-                            dxf.Entities.Add(new Circle(start, radius));
-                        }
-                        if (figure.TypeId == 3)
-                        {
-                            var start = new Vector2(coorditanes[0], coorditanes[1]);
-                            var radius = coorditanes[2];
-                            var startAngle = coorditanes[3];
-                            var endAngle = coorditanes[4];
-                            if (detail.Rotated)
-                            {
-                                (start.X, start.Y) = (-start.Y, start.X);
-                                (center.X, center.Y) = (-center.Y, center.X);
-                                (startAngle, endAngle) = (startAngle + 90, endAngle + 90);
-                            }
-                            start += new Vector2(detail.X, -detail.Y) + sizes - center;
-                            dxf.Entities.Add(new Arc(start, radius, startAngle, endAngle));
                         }
 
                     }
@@ -116,63 +76,39 @@ namespace vega.Services
             return answer;
         }
 
-        public async Task<List<FigureDTO>> GetDXFAsync(byte[] fileBytes)
+        public async Task<List<Figure>> GetDXFAsync(byte[] fileBytes)
         {
-            try
+            var ans = new List<Figure>();
+            using var stream = new MemoryStream(fileBytes);
+            var dxf = DxfDocument.Load(stream);
+            foreach (var obj in dxf.Entities.All)
             {
-
-                if (fileBytes == null || fileBytes.Length == 0)
+                if (obj is Line line)
+                    ans.Add(new Figure { TypeId = 1, Coordinates = $"{line.StartPoint.X}; {line.StartPoint.Y}; {line.EndPoint.X}; {line.EndPoint.Y}" });
+                if (obj is Circle circle)
+                    ans.Add(new Figure { TypeId = 2, Coordinates = $"{circle.Center.X}; {circle.Center.Y}; {circle.Radius}" });
+                if (obj is Arc arc)
+                    ans.Add(new Figure { TypeId = 3, Coordinates = $"{arc.Center.X}; {arc.Center.Y}; {arc.Radius}; {arc.StartAngle}; {arc.EndAngle}" });
+                if (obj is Spline spline)
                 {
-                    throw new ArgumentException("File bytes cannot be empty");
+                    var coordinates = new StringBuilder();
+                    foreach (var item in spline.ControlPoints)
+                        coordinates.Append($"{item.ToString()}/");
+                    ans.Add(new Figure { TypeId = 4, Coordinates = coordinates.ToString() });
                 }
 
-                using var content = new MultipartFormDataContent();
-                var fileContent = new ByteArrayContent(fileBytes);
-                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
-                content.Add(fileContent, "file", "drawing.dxf");
-
-                var response = await _httpClient.PostAsync("parse-dxf", content);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    var errorContent = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine($"DXF API error: {response.StatusCode}, {errorContent}");
-                    throw new Exception($"DXF parsing failed: {errorContent}");
-                }
-
-                var responseStream = await response.Content.ReadAsStreamAsync();
-                var options = new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true,
-                    AllowTrailingCommas = true
-                };
-
-                var result = await JsonSerializer.DeserializeAsync<List<FigureDTO>>(responseStream, options);
-
-                if (result == null || result.Count == 0)
-                {
-                    throw new Exception("No valid figures found in DXF file");
-                }
-
-                return result;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message, "Error in GetDXFAsync");
-                throw new Exception("Failed to parse DXF", ex);
-            }
+            return ans;
         }
-
-
         private Vector2 GetDetailCenter(List<Figure> figures, float detailX = 0, float detailY = 0)
         {
-            var maxX = figures.Max(f => float.Parse(f.Coordinates.Split(';')[0], new CultureInfo("ru-RU")));
-            var maxY = figures.Max(f => float.Parse(f.Coordinates.Split(';')[1], new CultureInfo("ru-RU")));
-            var minX = figures.Min(f => float.Parse(f.Coordinates.Split(';')[0], new CultureInfo("ru-RU")));
-            var minY = figures.Min(f => float.Parse(f.Coordinates.Split(';')[1], new CultureInfo("ru-RU")));
+            var maxX = figures.Max(f => float.Parse(f.Coordinates.Split(';')[0]));
+            var maxY = figures.Max(f => float.Parse(f.Coordinates.Split(';')[1]));
+            var minX = figures.Min(f => float.Parse(f.Coordinates.Split(';')[0]));
+            var minY = figures.Min(f => float.Parse(f.Coordinates.Split(';')[1]));
 
-            var detailCenterX = (((minX + maxX) / 2) + detailX);
-            var detailCenterY = (((minY + maxY) / 2) + detailY);
+            var detailCenterX = (int)(((minX + maxX) / 2) + detailX);
+            var detailCenterY = (int)(((minY + maxY) / 2) + detailY);
 
             return new Vector2(detailCenterX, detailCenterY);
         }
